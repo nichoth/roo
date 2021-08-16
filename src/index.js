b08cc06475eed062f855a6d24bc6ba05ab4b30a7
nichoth <nichoth@gmail.com>
2021-08-16 14:35:00 -0700

    no br package

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
-// eslint-disable-next-line
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom'
 // import { html } from 'htm/react'
-var Bus = require('@nichoth/events')
-var subscribe = require('./subscribe')
-var router = require('./routes')
-var route = require('route-event')()
-var state = require("./state")
+// var Bus = require('@nichoth/events')
+// var subscribe = require('./subscribe')
+// var router = require('./routes')
+// var route = require('route-event')()
+// var state = require("./state")
 
-var bus = Bus({ memo: true })
-subscribe(bus, state)
+// var bus = Bus({ memo: true })
+// subscribe(bus, state)
 
-function Connector () {
-    var [_state, setState] = useState(state())
-    var { path } = _state
+// function Connector () {
+//     var [_state, setState] = useState(state())
+//     var { path } = _state
 
-    // connect our `state` object to the component state
-    state(function onChange (newState) {
-        setState(newState)
-    })
+//     // connect our `state` object to the component state
+//     state(function onChange (newState) {
+//         setState(newState)
+//     })
 
-    // find which view to use for this path
-    var match = router.match(path)
-    if (!match) return null
-    var { view } = match.action(match)
-    var emit = bus.emit.bind(bus)
+//     // find which view to use for this path
+//     var match = router.match(path)
+//     if (!match) return null
+//     var { view } = match.action(match)
+//     var emit = bus.emit.bind(bus)
 
-    return html`<${view} ...${_state} emit=${emit}
-        setRoute=${route.setRoute}
-    />`
-}
+//     return html`<${view} ...${_state} emit=${emit}
+//         setRoute=${route.setRoute}
+//     />`
+// }
 
-route(function onRoute (path) {
-    // we keep the route path as a part of state
-    // when we set the state here it causes a re-render
-    state.path.set(path)
-})
+// route(function onRoute (path) {
+//     // we keep the route path as a part of state
+//     // when we set the state here it causes a re-render
+//     state.path.set(path)
+// })
 
 function test () {
     return <p>test</p>;
